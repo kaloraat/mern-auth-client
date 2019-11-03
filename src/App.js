ec0592dc0c8ed945d1975e51eea84f32485f276e
ryandcsg <ryand@gmail.com>
2019-11-03 15:07:19 +1100

    deployment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Layout from './core/Layout';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Layout>
+            <div className="col-md-6 offset-md-3 text-center">
+                <h1 className="p-5">React Node MongoDB Authentication Boilerplate</h1>
+                <h2>MERN STACK</h2>
+                <hr />
+                <p className="lead">
+                    MERN stack login register system with account activation, forgot password, reset password, login
+                    with facebook and google as well as private and protected routes for authenticated user and users
+                    with the role of admin.
+                </p>
+                <h4 className="p2">Bonus</h4>
+                <p className="lead">Profile update & deployment to digital ocean cloud servers</p>
+            </div>
+        </Layout>
+    );
+};
 
 export default App;
