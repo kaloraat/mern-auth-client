3bdfe0c244a41d42e6cc71f69646b0f4338c9670
ryandcsg <ryand@gmail.com>
2019-11-29 21:14:53 +1100

    static site generate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Layout from './core/Layout';
+import { Helmet } from 'react-helmet';
 
 const App = () => {
+    const head = () => (
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>MERN Stack</title>
+            <link rel="canonical" href="https://mern-stack.com" />
+        </Helmet>
+    );
     return (
         <Layout>
+            {head()}
             <div className="col-md-6 offset-md-3 text-center">
                 <h1 className="p-5">React Node MongoDB Authentication Boilerplate</h1>
                 <h2>MERN STACK</h2>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Routes from './Routes';
+
+// ReactDOM.render(<Routes />, document.getElementById('root'));
+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { hydrate, render } from 'react-dom';
 import Routes from './Routes';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (rootElement.hasChildNodes()) {
+    hydrate(<Routes />, rootElement);
+} else {
+    render(<Routes />, rootElement);
+}
